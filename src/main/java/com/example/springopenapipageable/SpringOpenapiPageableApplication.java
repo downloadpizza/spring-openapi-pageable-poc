c07830e8f8a0ea2a21168d1312de3ecdafea4e8e
DownloadPizza <klenngabriel@gmail.com>
2022-08-01 17:27:59 +0200

    Indeed

---
+++ src/main/java/com/example/springopenapipageable/SpringOpenapiPageableApplication.java
@@ -0,0 +1,13 @@
+package com.example.springopenapipageable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenapiPageableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOpenapiPageableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springopenapipageable/SpringOpenapiPageableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springopenapipageable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenapiPageableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
